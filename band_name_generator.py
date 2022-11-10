5a2bafbebc124a916f79744f7461004a7c90e176
brandocoding <41130732+brandocoding@users.noreply.github.com>
2022-11-10 07:47:10 -0600

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,8 @@
+# Create a greeting for your program
+print("Welcome to Band Name Generator")
+# Ask the user for the city that they grew up in.
+a = input("What city did you grow up in?\n")
+# Ask the user for the name of a pet growing up.
+b = input("What was the name of a pet you had growing up?\n")
+# combine the names to create their band name.
+print("Your generated band name is " + a + " " + b)
